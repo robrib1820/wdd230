eeebc9f99eff12cd8fe22d04d641e600ab990c49
robrib1820 <rib21006@byui.edu>
2022-10-22 04:39:25 -0300

    ups

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,18 +1,18 @@
-const tC = 5   ;
 const skmH = 10;
+const tempC = 5;
 
-document.querySelector('#data_weather').textContent = tC;
+document.querySelector('#data_weather').textContent = tempC;
 document.querySelector('#wind_speed').textContent = skmH;
 
-const tF = tC * (9/5) + 32;
+const tempF = tempC * (9/5) + 32;
 const smH = skmH / 1.609;
 
-console.log(tF);
+console.log(tempF);
 console.log(smH);
 
-if (tF <= 50 && smH > 3) {
-const f = 35.74 + 0.6215 * tF - 35.75 * (smH**0.16) + 0.4275 * tF * (smH**0.16)
-document.querySelector('#wind_chill').textContent = f.toFixed(1);
+if (tempF <= 50 && smH > 3) {
+const fah = 35.74 + 0.6215 * tempF - 35.75 * (smH**0.16) + 0.4275 * tempF * (smH**0.16)
+document.querySelector('#wind_chill').textContent = fah.toFixed(1);
 }
 else {
   document.querySelector('#wind_chill').textContent = 'N/A';
