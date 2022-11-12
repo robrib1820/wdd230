75e75d3b70e75865ecfbb8df1f1f9c34707f270a
robrib1820 <rib21006@byui.edu>
2022-11-12 00:28:35 -0300

    ups

--- chamber/scripts/data.js
+++ chamber/scripts/data.js
@@ -1,5 +1,6 @@
 const requestFile = './scripts/data.json';
 const carDs = document.querySelector('.cards');
+const lists = document.querySelector('.lists');
 
 fetch(requestFile)
   .then(function (response) {
@@ -14,11 +15,22 @@ fetch(requestFile)
   function displayCards(card) {
     // Create elements to add to the document
     let cards = document.createElement('section');
+    let lists = document.createElement('section');
+
     let name = document.createElement('h2');
+    let lName = document.createElement('h2');
+    
     let address = document.createElement('p');
+    let lAddress = document.createElement('p');
+
     let phoNe = document.createElement('p');
+    let lPhone = document.createElement('p');
+    
     let url = document.createElement('p');
+    let lUrl = document.createElement('p');
+
     let mebLevel = document.createElement('p');
+    let lmebLevel = document.createElement('p');
     let portrait = document.createElement('img');
 
 
@@ -29,6 +41,12 @@ fetch(requestFile)
     phoNe.textContent = card.phone;
     url.textContent = card.URL;
     mebLevel.textContent = card.membershipLevel;
+    //list content
+    lName.textContent = card.name;
+    lAddress.textContent = card.addresses;
+    lPhone.textContent = card.phone;
+    lUrl.textContent = card.URL;
+    lmebLevel.textContent = card.membershipLevel;
     
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
     portrait.setAttribute('src', card.imageurl);
@@ -42,9 +60,28 @@ fetch(requestFile)
     cards.appendChild(phoNe);
     cards.appendChild(url);
     cards.appendChild(mebLevel);
+
+    // Append list elements
+    lists.appendChild(lName);
+    lists.appendChild(lAddress);
+    lists.appendChild(lPhone);
+    lists.appendChild(lUrl);
+    lists.appendChild(lmebLevel);
     
     
   
     // Append the cards
     document.querySelector('div.cards').appendChild(cards);
+    document.querySelector('div.lists').appendChild(lists);
   }
+
+function view(option){
+  if (option == 'large'){
+      document.querySelector(".cards").style.display=""; 
+      document.querySelector(".lists").style.display="none"; 
+        
+  }else if (option == "small"){
+    document.querySelector(".lists").style.display="";
+    document.querySelector(".cards").style.display="none";
+  }
+}
